5b5574fb085c716c20a5a933c8642506a2159f31
evanmacbride <ejmacbride@gmail.com>
2020-09-20 14:47:01 -0400

    fix how nodes are inserted and labeled, add micro dataset

--- id3_evan.py
+++ id3_evan.py
@@ -10,6 +10,12 @@ class Node:
         self.right = None
         self.label = label
 
+    def __repr__(self):
+        return '{label:' + str(self.label) + '}'
+
+    def __str__(self):
+        return str(self.label)
+
     def insert(self, label):
         if self.left is None:
             self.left = Node(label)
@@ -19,18 +25,29 @@ class Node:
             sys.stderr.write('Error: Cannot insert. All branches at node are set.\n')
             sys.exit()
 
+    def printout(self, node):
+        tree = ''
+        if node:
+            tree += str(node.label)
+            if (self.left):
+                tree += self.printout(node.left)
+            if (self.right):
+                tree += self.printout(node.right)
+        return tree
+
     def __str__(self, offset=''):
         """ TODO: Get this working properly. """
+        """
+        #print(offset + str(self.label))
+        #print(self.label)
         if self.left:
-            print(":0")
-            offset = offset + "-"
+            #offset = offset + "-"
             print(self.left, offset)
-        print(offset + str(self.label))
-        if self.right:
-            print(":1")
-            offset = offset + "-"
+        elif self.right:
+            #offset = offset + "-"
             print(self.right, offset)
-        return label
+        """
+        return str(self.label)
 
 class Attribute:
     def __init__(self, label):
@@ -49,6 +66,15 @@ class Attribute:
     def getTotal(self):
         return self.getTotalVal0() + self.getTotalVal1();
 
+def printy(node):
+    if node:
+        print(node)
+        if node.left:
+            printy(node.left)
+        if node.right:
+            printy(node.right)
+    return
+
 # Get the label (i.e. attribute name) that has the highest information gain
 def information_gain_heuristic(s, attr_dict):
     gain_list = []
@@ -60,9 +86,9 @@ def information_gain(s, attr):
     # The entropy of the entire set
     entropy_s = get_entropy(s.label, s.getTotalVal1(), s.getTotalVal0(), s.getTotal())
     # The entropy of the subset that has "0" as a value for the attribute
-    entropy_attr_0 = get_entropy(attr.label + "0", attr.val0_pos, attr.val0_neg, attr.getTotalVal0())
+    entropy_attr_0 = get_entropy(attr.label, attr.val0_pos, attr.val0_neg, attr.getTotalVal0())
     # The entropy of the subset that has "1" as a value for the attribute
-    entropy_attr_1 = get_entropy(attr.label + "1", attr.val1_pos, attr.val1_neg, attr.getTotalVal1())
+    entropy_attr_1 = get_entropy(attr.label, attr.val1_pos, attr.val1_neg, attr.getTotalVal1())
     # Subtract from the set's entropy the entropy of each value multiplied by its proporition in the set
     gain = entropy_s[1] - (attr.getTotalVal0()/attr.getTotal()) * entropy_attr_0[1] - (attr.getTotalVal1()/attr.getTotal()) * entropy_attr_1[1]
     # Return the label of the attribute and its gain
@@ -70,8 +96,9 @@ def information_gain(s, attr):
 
 def get_entropy(node_label, val1_instances, val0_instances, total):
     """The formula for entropy is Entropy = -p_1 * log_2(p_1) - p_0 * log_2(p_0)"""
-    entropy = (-1 * val1_instances/total * math.log2(val1_instances/total)) - \
-              (val0_instances/total * math.log2(val0_instances/total))
+    """As per Mitchell p. 56, 0 log 0 is defined as zero """
+    entropy = (-1 * val1_instances/total * math.log2(val1_instances/total if val1_instances/total > 0 else 1)) - \
+              (val0_instances/total * math.log2(val0_instances/total if val0_instances/total > 0 else 1))
     return node_label, entropy
 
 # examples_list     A pandas dataframe
@@ -80,12 +107,10 @@ def get_entropy(node_label, val1_instances, val0_instances, total):
 def id3(examples_list, target_attribute, attributes_list):
     root = Node()
     A = ''
-    # If all examples are positive, set root's label to "+"
-    #if (all(example[-1] == 1 for example in examples_list)):
+    # If all examples are positive, set root's label to "1"
     if (all(l == 1 for l in list(examples_list[target_attribute]))):
         root.label = 1
-    # Elif all examples are negative, set root's label to "-"
-    #elif (all(example[-1] == 0 for example in examples_list)):
+    # Elif all examples are negative, set root's label to "0"
     elif (all(l == 0 for l in list(examples_list[target_attribute]))):
         root.label = 0
     # Elif attributes_list is empty, set root's label to most common value of
@@ -98,7 +123,9 @@ def id3(examples_list, target_attribute, attributes_list):
         #class_attr = attr_dict[target_attribute]
         #del attr_dict[target_attribute]
         #A = information_gain_heuristic(class_attr, attr_dict)
+        root.label = A
         for i in [0,1]:
+            '''
             new_node = Node()
             examples_list_vi = examples_list.loc[examples_list[A] == i]
             if (examples_list_vi.empty):
@@ -109,6 +136,20 @@ def id3(examples_list, target_attribute, attributes_list):
                 trimmed_attributes = copy.deepcopy(attributes_list) # Do I really need deepcopy?
                 trimmed_attributes.remove(A)
                 root.insert(id3(examples_list_vi,target_attribute,trimmed_attributes))
+            '''
+            new_branch = Node()
+            new_branch.label = str(i)
+            examples_list_vi = examples_list.loc[examples_list[A] == i]
+            if (examples_list_vi.empty):
+                new_leaf = Node()
+                new_leaf.label = examples_list[target_attribute].mode()[0]
+                new_branch.insert(new_leaf)
+                root.insert(new_branch)
+            else:
+                trimmed_attributes = copy.deepcopy(attributes_list) # Do I really need deepcopy?
+                trimmed_attributes.remove(A)
+                new_branch.insert(id3(examples_list_vi,target_attribute,trimmed_attributes))
+                root.insert(new_branch)
     return root
 
 def get_example_data(dataset):
@@ -161,6 +202,9 @@ train_df = pd.read_csv(training_set)
 validation_df = pd.read_csv(validation_set)
 test_df = pd.read_csv(test_set)
 
+micro_set = 'data_sets2/data_sets2/micro_set.csv'
+micro_df = pd.read_csv(micro_set)
+
 #attribute_labels_list, val1_instances_list, val0_instances_list, total_instances_list = get_attribute_labels(train_df)
 #print(attribute_labels_list)
 #print(val1_instances_list)
@@ -184,5 +228,7 @@ for label, gain in gain_list:
 print(information_gain_heuristic(class_attr, attr_dict))
 """
 
-tree = id3(train_df, "Class", list(train_df.columns[0:-1]))
-#print(tree)
+#tree = id3(train_df, "Class", list(train_df.columns[0:-1]))
+tree = id3(micro_df, "Class", list(micro_df.columns[0:-1]))
+#print(tree.printout(tree))
+print(tree)
